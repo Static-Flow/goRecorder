c1aa7ac0b5d4e1b69f7050aed6bdc8bcfa0b21b8
Tanner Barnes <tannerbarnes@Tanners-MacBook-Pro.local>
2019-03-24 09:22:09 -0500

    Added functionality to list availablescreens and specify which ones you want to record

--- recorder.go
+++ recorder.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"flag"
 	"time"
+	"strconv"
+	"strings"
 	term "github.com/nsf/termbox-go"
 )
 
@@ -15,93 +17,120 @@ func reset() {
 	term.Sync()
 }
 
+func splitScreens(ids string) []int {
+	idList := strings.Split(ids, ",")
+	idIntList := []int{}
+    for _, i := range idList {
+        j, err := strconv.Atoi(i)
+        if err != nil {
+            panic(err)
+        }
+        idIntList = append(idIntList, j)
+ 	}
+ 	return idIntList
+}
+
+func getScreenInfo(){
+	screensInfo := ""
+	n := screenshot.NumActiveDisplays()
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+		screensInfo += fmt.Sprintf("%d: %d by %d Monitor\n",i,bounds.Dx(),bounds.Dy())
+	}
+	fmt.Println(screensInfo)
+}
+
 func main() {
 
-	screenPtr := flag.Int("numScreens",screenshot.NumActiveDisplays(), "Number of screens to use")
+	screenPtr := flag.String("screenIds","", "list of screen id to use, e.g. 0,1,n. Use -listScreens to see available ids")
 	pollingPtr := flag.Int("pollingInt", 1, "How many seconds between screen grabs")
 	bufferSizePtr := flag.Int("bufferSize", 60, "How many pictures to store in memory per screen")
+	listScreensPtr := flag.Bool("listScreens",false,"List Screen Info")
 	flag.Parse()
-
-	var pics [][]*image.RGBA
-	n := *screenPtr
-	ticker := time.NewTicker(time.Duration(*pollingPtr) * time.Second)
-	defer ticker.Stop()
-	finished := make(chan bool)
-	pause := false
-	output := false
-	previousNow := time.Now()
-	err := term.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer term.Close()
-	go func() {
-		for {
-			switch ev := term.PollEvent(); ev.Type {
-				case term.EventKey:
-					switch ev.Key {
-						case term.KeyEsc:
-							reset()
-							fmt.Println("Exiting")
-							finished <- true
-							return
-						case term.KeyF1:
-							reset()
-							fmt.Println("Compiling Images")
-							output = true
-						case term.KeyF2:
-							reset()
-							if pause {
-								pause = false
-								fmt.Println("Resuming")
-							} else {
-								pause = true
-								fmt.Println("Pausing")
-							}
-						default:
-							reset()
-					}
-			}
+	if *listScreensPtr {
+		getScreenInfo()
+	} else{
+		var pics [][]*image.RGBA
+		n := splitScreens(*screenPtr)
+		ticker := time.NewTicker(time.Duration(*pollingPtr) * time.Second)
+		defer ticker.Stop()
+		finished := make(chan bool)
+		pause := false
+		output := false
+		previousNow := time.Now()
+		err := term.Init()
+		if err != nil {
+			panic(err)
 		}
-	}()
-	for {
-		if output {
-			for _, picArray := range pics {
-				for index, pic := range picArray {
-					fileName := fmt.Sprintf("%d_%d-%02d-%02dT%02d.%02d.%02d.png",index, 
-        previousNow.Year(), previousNow.Month(), previousNow.Day(),
-        previousNow.Hour(), previousNow.Minute(), previousNow.Second())
-					file, err := os.Create(fileName)
-					if err != nil {
-						panic(err)
-					}
-					defer file.Close()
-					png.Encode(file, pic)
+		defer term.Close()
+		go func() {
+			for {
+				switch ev := term.PollEvent(); ev.Type {
+					case term.EventKey:
+						switch ev.Key {
+							case term.KeyEsc:
+								reset()
+								fmt.Println("Exiting")
+								finished <- true
+								return
+							case term.KeyF1:
+								reset()
+								fmt.Println("Compiling Images")
+								output = true
+							case term.KeyF2:
+								reset()
+								if pause {
+									pause = false
+									fmt.Println("Resuming")
+								} else {
+									pause = true
+									fmt.Println("Pausing")
+								}
+							default:
+								reset()
+						}
 				}
 			}
-			output = false
-		} else if !pause {
-			select {
-				case <-finished:
-					ticker.Stop()
-					return
-				case t := <-ticker.C:
-					previousNow = t
-					fmt.Println("Current time: ", t.UTC().Format(time.UnixDate))
-					var screenPics []*image.RGBA
-					for i := 0; i < n; i++ {
-			 			bounds := screenshot.GetDisplayBounds(i)
+		}()
+		for {
+			if output {
+				for _, picArray := range pics {
+					for index, pic := range picArray {
+						fileName := fmt.Sprintf("%d_%d-%02d-%02dT%02d.%02d.%02d.png",index, 
+	        			previousNow.Year(), previousNow.Month(), previousNow.Day(),
+				        previousNow.Hour(), previousNow.Minute(), previousNow.Second())
+						file, err := os.Create(fileName)
+						if err != nil {
+							panic(err)
+						}
+						defer file.Close()
+						png.Encode(file, pic)
+					}
+				}
+				output = false
+			} else if !pause {
+				select {
+					case <-finished:
+						ticker.Stop()
+						return
+					case t := <-ticker.C:
+						previousNow = t
+						fmt.Println("Current time: ", t.UTC().Format(time.UnixDate))
+						var screenPics []*image.RGBA
+						for i := 0; i < len(n); i++ {
+				 			bounds := screenshot.GetDisplayBounds(n[i])
 
-				 		img, err := screenshot.CaptureRect(bounds)
-				 		if err != nil {
-			 				panic(err)
-			 			}
-			 			screenPics = append(screenPics,img)
-				 	}
-				 	if len(pics) == *bufferSizePtr{
-			 			pics = pics[1:]
-			 		}
-				 	pics = append(pics,screenPics)
+					 		img, err := screenshot.CaptureRect(bounds)
+					 		if err != nil {
+				 				panic(err)
+				 			}
+				 			screenPics = append(screenPics,img)
+					 	}
+					 	if len(pics) == *bufferSizePtr{
+				 			pics = pics[1:]
+				 		}
+					 	pics = append(pics,screenPics)
+				}
 			}
 		}
 	}
